c051c92db705e435ae0d41b89034abbef1656f0f
Вова <v.grishchenko@onpoint.ru>
2026-02-21 14:25:13 +0300

    убрал стандартную разметку, начал создавать разметку приложения

---
+++ src/components/layouts/MainLayout.vue
@@ -0,0 +1,106 @@
+<script setup lang="ts">
+import IconSunny from '../icons/IconSunny.vue';
+
+
+</script>
+
+<template>
+  <div class="wrapper">
+
+      <div class="leftSide">
+        <div class="headingBlock">
+          <div class="mainHead">Moskow Weather</div>
+          <div class="secondaryHead">Russia Moskow</div>
+          <div class="weatherHeadBlock">
+            <div class="weatherBlock">
+              <div class="weatherIcon">
+                <IconSunny/>
+              </div>
+              <div class="temperature">
+                14 C
+              </div>
+            </div>
+            <div class="weatherdescription">
+              cloudy
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="rightSide">
+        <div class="dateHead">
+            <div class="mainHead">May 2023</div>
+          <div class="secondaryHead">Friday, May 19, 2023</div>
+        </div>
+        <div class="dateWeatherBlock">
+          <div class="dateWeatherItem">
+            <div class="dateWeatherItem-day">San</div>
+            <IconSunny/>
+            <div class="dateWeatherItem-weather">14 C</div>
+          </div>
+          <div class="dateWeatherItem">
+            <div class="dateWeatherItem-day">San</div>
+            <IconSunny/>
+            <div class="dateWeatherItem-weather">14 C</div>
+          </div>
+          <div class="dateWeatherItem">
+            <div class="dateWeatherItem-day">San</div>
+            <IconSunny/>
+            <div class="dateWeatherItem-weather">14 C</div>
+          </div>
+        </div>
+      </div>
+  </div>
+
+</template>
+
+<style scoped>
+.wrapper{
+  background-color: #101014;
+  display: grid;
+  height: 100vh;
+  width: 100vw;
+  grid-template-areas: "leftSide rightSide";
+  grid-template-columns: 1fr 3fr;
+}
+.leftSide{
+  grid-area: 'leftSide';
+}
+.rightSide{
+  grid-area: 'rightSide';
+}
+.headingBlock, .dateHead{
+  padding: 20px;
+}
+.mainHead{
+  color: #fff;
+  font-size: larger;
+  font-weight: 600;
+}
+.secondaryHead{
+  color: #ffffff96;
+  font-size:  medium;
+  font-weight: 400;
+}
+.weatherHeadBlock{
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+  align-items: center;
+}
+.weatherBlock{
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+}
+.dateWeatherBlock{
+  display: flex;
+  gap: 15px;
+}
+.dateWeatherItem{
+  background-color: #ffffff4b;
+  padding: 10px;
+  border-radius: 15px;
+  text-align: center;
+}
+</style>
